7f7d58df17c48cf34d7cf55c2606fde68e572033
Raye Rayne <247046349+21leahcimhtiek-oss@users.noreply.github.com>
2026-04-04 16:31:06 -0700

    feat: add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+/** @type {import("jest").Config} */
+const customJestConfig = {
+  setupFilesAfterFramework: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testMatch: [
+    "**/__tests__/**/*.test.ts",
+    "**/__tests__/**/*.test.tsx",
+  ],
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/app/layout.tsx",
+  ],
+  coverageThreshold: {
+    global: { branches: 60, functions: 60, lines: 60, statements: 60 },
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
