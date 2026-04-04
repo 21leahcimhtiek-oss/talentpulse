62a1281eaa05f3da4d85e7b48a8f40e1024ccac0
Raye Rayne <247046349+21leahcimhtiek-oss@users.noreply.github.com>
2026-04-04 16:29:02 -0700

    feat: add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "*.supabase.co" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["openai"],
+  },
+};
+
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  silent: true,
+  widenClientFileUpload: true,
+  hideSourceMaps: true,
+  disableLogger: true,
+});
